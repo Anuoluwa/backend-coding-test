39a611b235476414717ac0c942d52b4ae95bdeb8
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2021-08-30 00:14:13 +0100

    complete task 3 with test and documentations

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
 const sqlite3 = require("sqlite3").verbose();
+const assert = require("assert");
+const { expect } = require("chai");
 
 const db = new sqlite3.Database(":memory:");
 
@@ -29,7 +31,7 @@ describe("API tests", () => {
   });
   describe("POST /rides, to create a new ride", () => {
     describe("POST /rides", () => {
-      it("should return error when start or end latitude is not between -90 or 90", (done) => {
+      it("returns error when start or end latitude is not between -90 or 90", (done) => {
         request(app)
           .post("/rides")
           .send({
@@ -75,7 +77,7 @@ describe("API tests", () => {
           });
       });
 
-      it("should responds with json with a valid data", (done) => {
+      it("should return numbers for latitude and longitude values", (done) => {
         request(app)
           .post("/rides")
           .send({
@@ -89,20 +91,63 @@ describe("API tests", () => {
           })
           .set("Accept", "application/json")
           .expect("Content-Type", /json/)
-          .expect((res) => {
-            res.body.rideID = 1;
-            res.body.startLat = 89;
-            res.body.startLong = 179;
-            res.body.endLat = -88;
-            res.body.endLong = 101;
-            res.body.riderName = "John Jane";
-            res.body.driverName = "John Doe";
-            res.body.driverVehicle = "Jeep";
-          })
-          .end((err) => {
-            if (err) return done(err);
-            done();
+          .end((err, res) => {
+            expect(res.body[0].rideID).to.be.a("number");
+            expect(res.body[0].startLat).to.be.a("number");
+            expect(res.body[0].startLong).to.be.a("number");
+            expect(res.body[0].endLat).to.be.a("number");
+            expect(res.body[0].endLong).to.be.a("number");
           });
+        done();
+      });
+
+      it("should return string of values for rider's ad driver's data", (done) => {
+        request(app)
+          .post("/rides")
+          .send({
+            start_lat: "-89",
+            end_lat: "-88",
+            start_long: "179",
+            end_long: "101",
+            rider_name: "John Jane",
+            driver_name: "John Doe",
+            driver_vehicle: "Jeep",
+          })
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            expect(res.body[0].riderName).to.be.a("string");
+            expect(res.body[0].driverName).to.be.a("string");
+            expect(res.body[0].driverVehicle).to.be.a("string");
+            expect(res.body[0].created).to.be.a("string");
+          });
+        done();
+      });
+
+      it("should return same values as that of the request data", (done) => {
+        request(app)
+          .post("/rides")
+          .send({
+            start_lat: "-89",
+            end_lat: "-88",
+            start_long: "179",
+            end_long: "101",
+            rider_name: "John Jane",
+            driver_name: "John Doe",
+            driver_vehicle: "Jeep",
+          })
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            expect(res.body[0].startLat).to.deep.equal(-89);
+            expect(res.body[0].startLong).to.deep.equal(179);
+            expect(res.body[0].endLat).to.deep.equal(-88);
+            expect(res.body[0].endLong).to.deep.equal(101);
+            expect(res.body[0].riderName).to.deep.equal("John Jane");
+            expect(res.body[0].driverName).to.deep.equal("John Doe");
+            expect(res.body[0].driverVehicle).to.deep.equal("Jeep");
+          });
+        done();
       });
       it("should return 1 for object length with a valid data", (done) => {
         request(app)
@@ -129,16 +174,71 @@ describe("API tests", () => {
     });
   });
 
-  describe("GET /rides return all available rides", () => {
+  describe("GET /rides return all available rides with pagination", () => {
     it("responds with json", (done) => {
       request(app)
         .get("/rides")
         .expect("Content-Type", /json/)
         .expect(200, done);
     });
+    it("return an array of object(s) and not undefined", (done) => {
+      request(app)
+        .get(`/rides/?page=${1}&limit=${10}`)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          expect(res.body).to.not.be.an("undefined");
+        });
+      done();
+    });
+    it("return an array of length greater than zero", (done) => {
+      request(app)
+        .get(`/rides/?page=${1}&limit=${10}`)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          // eslint-disable-next-line no-unused-expressions
+          expect(res.body).to.not.empty;
+        });
+      done();
+    });
+    it("return an array of object(s)", (done) => {
+      request(app)
+        .get(`/rides/?page=${1}&limit=${10}`)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          expect(res.body).to.be.an("array");
+        });
+      done();
+    });
     it("should return the value of 1 for the numbers of rows in the response object", (done) => {
       request(app)
-        .get(`/rides/${1}`)
+        .get(`/rides?page=${1}&limit=${10}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body.length).toBe(1);
+        });
+      done();
+    });
+    it("should return riderId of the first row that includes 1 for any page e.g 1, 11, 21, 31", (done) => {
+      request(app)
+        .get(`/rides?page=${1}&limit=${10}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body[0].riderId).to.include("1");
+        });
+      done();
+    });
+    it("should return riderId of the first row that includes 1 for any page e.g 1, 11, 21, 31", (done) => {
+      request(app)
+        .get(`/rides?page=${1}&limit=${10}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body[0].riderId).to.include("0");
+        });
+      done();
+    });
+    it("should return the value of 1 for the numbers of rows in the response object", (done) => {
+      request(app)
+        .get(`/rides?page=${1}&limit=${10}`)
         .expect(200)
         .then((res) => {
           expect(res.body.length).toBe(1);
